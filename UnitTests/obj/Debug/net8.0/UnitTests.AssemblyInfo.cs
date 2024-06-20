322b8dfd75ff3f941d8b599207c892b50296c49a
Toni <tivanovic21@student.foi.hr>
2024-06-21 00:18:59 +0200

    Add dll changes

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74807d45116ab4d9f806fe75c3b86ce95eeb789c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26861bc8967995cb124697954afe5e54ab17c1ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
